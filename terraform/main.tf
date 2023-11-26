895eab85c0d516e1f82e0cff8422e0b4c6fb1519
ellis lee <ellismckenzielee@gmail.com>
2023-11-26 15:54:53 +0000

    init terraform

---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "../authorize.zip"
+  function_name = "authorize"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_handler.handler"
+  runtime = "python3.9"
+}
+
+
+
